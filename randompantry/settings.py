c7c2844750200d470fbf92bdcdd5cc77c27448be
irkaal <irkaalv@gmail.com>
2020-03-17 17:35:33 -0700

    Remove XFrameOptions header

--- randompantry/settings.py
+++ randompantry/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'randompantry.urls'
