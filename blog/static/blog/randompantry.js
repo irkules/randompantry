57c4a4adbb36b4fec935c754b951f1561197295a
irkaal <irkaalv@gmail.com>
2020-04-05 01:13:19 -0700

    Replace slider with slick slider and update UI

---
+++ blog/static/blog/randompantry.js
@@ -0,0 +1,84 @@
+const slickSettings = {
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+};
+
+const recSlickSettings = {
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ],
+    asNavFor: '.recipe-slider-focus'
+};
+
+const focusSlickSettings = {
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.recipe-slider-recommended'
+};
+
+$(window).scroll(function() {
+    if (window.scrollY > 150) {
+        $('.navbar.fixed-top').css('background-color', '#141414');
+    } else {
+        $('.navbar.fixed-top').css('background-color', '');
+    }
+});
+
+$(document).ready(function() {
+    $(`a[href='${window.location.pathname}']`).addClass('active');
+    $('.recipe-slider-focus').slick(focusSlickSettings);
+    $('.recipe-slider-recommended').slick(recSlickSettings);
+    $('.recipe-slider').slick(slickSettings);
+    $('.loading-animation').fadeOut(2000);
+    $('.rp-about-img').fadeIn(2000);
+});
--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,12 @@ from django.shortcuts import render
 
 def home(request):
     context = HomeContent.get_home_context()
-    context['title'] = 'Random Pantry!'
+    context['title'] = 'RANDOM PANTRY'
     return render(request, 'blog/home.html', context)
 
 def recipe_detail_view(request, pk):
     context = RecipeDetailContent.get_recipe_detail_context(pk)
-    context['title'] = 'Random Pantry! - Recipe'
+    context['title'] = 'RANDOM PANTRY - Recipe'
     if request.method == 'POST':
         form = UserReviewForm(request.POST)
         if form.is_valid():
@@ -23,5 +23,5 @@ def recipe_detail_view(request, pk):
     return render(request, 'blog/recipe_detail.html', context)
 
 def about(request):
-    context = { 'title': 'Recipe - About' }
+    context = { 'title': 'RANDOM PANTRY - About' }
     return render(request, 'blog/about.html', context)
